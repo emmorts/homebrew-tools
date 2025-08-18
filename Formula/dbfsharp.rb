c732646c8377f557320c000f035afbf333a41690
Tomas Stropus <h.guzas@gmail.com>
2025-08-18 22:44:22 +0300

    bump to v0.2.4

--- Formula/dbfsharp.rb
+++ Formula/dbfsharp.rb
@@ -1,24 +1,24 @@
 class Dbfsharp < Formula
   desc "High-performance .NET library and CLI tool for reading dBASE (DBF) files"
   homepage "https://github.com/emmorts/dbfsharp"
-  version "0.2.3"
+  version "0.2.4"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/emmorts/dbfsharp/releases/download/v0.2.3/dbfsharp-osx-arm64.tar.gz"
-      sha256 "0b810de252b0531800ee8f1e0037c65bae8de161653729b6d0f2f9ae5845014f"
+      url "https://github.com/emmorts/dbfsharp/releases/download/v0.2.4/dbfsharp-osx-arm64.tar.gz"
+      sha256 "362caf11da0da812d8ed6b4a2c07e31750302edcca1c24de3f343fbd65e8dfd8"
     end
     on_intel do
-      url "https://github.com/emmorts/dbfsharp/releases/download/v0.2.3/dbfsharp-osx-x64.tar.gz"
-      sha256 "7588a1949e8907afeacd4c8b6f1f3330e4917d1f0a08c659bcd522be2fd0b4b3"
+      url "https://github.com/emmorts/dbfsharp/releases/download/v0.2.4/dbfsharp-osx-x64.tar.gz"
+      sha256 "27da22d7bd85fcf2d458881801a1a35dd4ed55a13d2b82867073f029897c340a"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/emmorts/dbfsharp/releases/download/v0.2.3/dbfsharp-linux-x64.tar.gz"
-      sha256 "ebf7606a29618d8cfd2a4e77d8e68edb83f7379291bacb45109040334963e945"
+      url "https://github.com/emmorts/dbfsharp/releases/download/v0.2.4/dbfsharp-linux-x64.tar.gz"
+      sha256 "712dd63b93902fdfe327039047a95434d3009d6f3118875ef5c5f10e08a5e035"
     end
   end
 
